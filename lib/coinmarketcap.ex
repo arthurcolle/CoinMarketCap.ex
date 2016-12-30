4fd01fc4a2efccc8cd0fca39c7d8cbb27d99b055
Arthur Colle <arthurcolle@gmail.com>
2016-12-30 14:24:03 -0800

    Now you can get data for just the currency you want - still hitting CoinMarketCap.com API even if you just want one currency (need to fix)

--- lib/coinmarketcap.ex
+++ lib/coinmarketcap.ex
@@ -4,4 +4,8 @@ defmodule CoinMarketCap do
     %HTTPoison.Response{body: body} = HTTPoison.get! "https://api.coinmarketcap.com/v1/ticker/"
     Poison.decode!(body)
   end
+
+  def specific_currency_data(currency_string) do
+    Enum.filter(CoinMarketCap.data_dump, fn(x) -> x["name"] == currency_string end )
+  end
 end
